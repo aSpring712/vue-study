f5bad2e793fa6d1acbe8431b6cbf2b2215b6d2ba
aSpring <rnjsqhal2001@naver.com>
2022-02-19 22:44:56 +0900

    2-4. 게시글 작성 폼 만들기(vuex store mapState 사용)

--- front/pages/index.vue
+++ front/pages/index.vue
@@ -1,5 +1,6 @@
 <template>
   <v-container>
+    <PostForm v-if="me" />
     <div>
       <PostCard />
       <PostCard />
@@ -11,20 +12,26 @@
 </template>
 <script>
 import PostCard from '../components/PostCard.vue'
+import PostForm from '../components/PostForm.vue'
     export default {
-    components: { PostCard },
-        data() {
-            return {
-                name: 'Nuxt.js',
-            }
-        },
-        // 해당 페이지에 head()가 없으면, layout defualt의 head를 사용함
-        // head() {
-        //     return {
-        //         title: '메인페이지',
-        //     }
-        // },
-    }
+      components: { PostCard, PostForm },
+      data() {
+          return {
+              name: 'Nuxt.js',
+          }
+      },
+      computed: {
+        me() {
+          return this.$store.state.users.me;
+        }
+      }
+      // 해당 페이지에 head()가 없으면, layout defualt의 head를 사용함
+      // head() {
+      //     return {
+      //         title: '메인페이지',
+      //     }
+      // },
+}
 </script>
 <style>
 </style>
--- front/store/posts.js
+++ front/store/posts.js
@@ -1,13 +1,26 @@
 // index.js 내 모듈
 // 게시글의 대한 정보
 export const state = () => ({
-    name: 'posts', // index.js가 아닌 모듈이기 때문에 this.$store.state.posts.name
+    // index.js가 아닌 모듈이기 때문에 this.$store.state.posts.mainPosts
+    mainPosts: [], 
 });
 
 // module의 mutation 사용법
 // this.$store.commit('posts/bye')
 export const mutations = {
-    BYE(state) {
-        state.name = 'goodbye posts';
+    // 동기적으로 변경
+    addMainPost(state, payload) {
+        state.mainPosts.unshift(payload); // 맨 앞에 추가
     }
 };
+
+// 이 actions를 미리 만들어 둔 이유 : 서버에 게시글 등록 요청을 보내기 위해
+export const actions = {
+    add({ commit }, payload) {
+        // 여기서 posts/addMainPost 라고 하지않는 이유는, 여기가 main이라는 것을 알기 때문에 굳이 같은 모듈 내에서는 붙이지 않음
+        commit('addMainPost', payload); // 기본적으로 { root: false }
+
+        // // 모듈간 서로 호출이 가능한데, 그런데 다른 모듈인 index에도 addMainPost가 있고, 그걸 사용하고 싶다면
+        // commit('addMainPost', payload, { root: true }); // 이렇게 해주면 index 모듈에 있는 addMainPost가 호출됨
+    }
+}
