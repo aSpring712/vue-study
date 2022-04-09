ec38843293f84ff727df4a5de515a1e6fa2a76f8
aSpring <spring@mvpick.net>
2022-04-09 14:40:28 +0900

    3-8. 기타 라우트 구현하기(누락 재커밋)

---
+++ front/pages/user/_id/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <!-- 해당 유저가 쓴 글들만 노출 -->
+  <v-container>
+    <div>
+      <PostCard v-for="p in mainPosts" :key="p.id" :post="p" />
+    </div>
+  </v-container>
+</template>
+<script>
+import PostCard from '~/components/PostCard.vue'
+    export default {
+      components: { PostCard },
+      data() {
+          return {
+              name: 'Nuxt.js',
+          }
+      },
+      fetch({ store }) {
+        store.dispatch('posts/loadPosts');
+      },
+      computed: {
+        me() {
+          return this.$store.state.users.me;
+        },
+        mainPosts() {
+          return this.$store.state.posts.mainPosts;
+        },
+        hasMorePost() {
+          return this.$store.state.posts.hasMorePost;
+        }
+      },
+      mounted() { 
+        window.addEventListener('scroll', this.onScroll);
+      },
+      beforeDestroy() {
+        window.removeEventListener('scroll', this.onScroll);
+      },
+      methods: {
+        onScroll() {
+          if(window.scrollY + document.documentElement.clientHeight > document.documentElement.scrollHeight - 300) {
+            if(this.hasMorePost) {
+              this.$store.dispatch('posts/loadPosts');
+            }
+          }
+        },
+      },
+}
+</script>
+<style>
+</style>
