986fb490143c9543ac8ea3d629d5b9cfafc3e4fb
aSpring <spring@mvpick.net>
2022-04-09 14:39:30 +0900

    3-8. 기타 라우트 구현하기

--- front/middleware/anonymous.js
+++ front/middleware/anonymous.js
@@ -2,6 +2,8 @@
 
 // 미들웨어를 통해 페이지에 접근하기 전에 검사 + 동작을 취할 수 있음
 export default function ({ store, redirect }) { // ( context )
+  // 로그인 한 상태에서 '회원가입' 클릭해서 해당 페이지에 접근 시 해당 Middleware에 걸리지만
+  // 주소창에서 url 직접 입력 시 새로고침 되므로 store가 초기화되어 로그인 풀려서 회원가입 페이지에 접근 가능함
   if (store.state.users.me) { // 로그인 한 사용자
     redirect('/'); // 메인 페이지로 보냄
   }
