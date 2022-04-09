aa04489cb11fc86a935132f04c8af6b769ffc9f5
aSpring <spring@mvpick.net>
2022-04-09 16:32:45 +0900

    4-1. 백엔드 코딩 준비하기(back 폴더, express 설치)

--- front/nuxt.config.js
+++ front/nuxt.config.js
@@ -4,8 +4,11 @@ module.exports = {
     },
     // 설치한 모듈들을 아래처럼 적어주면 자동으로 연결됨 (Vue.use 사용하지 않음) -> 모든 페이지에 알아서 적용됨 -> 중복 제거!
     modules: [
-        '@nuxtjs/vuetify',
         '@nuxtjs/axios', // 서버쪽에 요청을 ajax로 보내기 위한 패키지
+    ],
+    // 원래는 devModules에 적어주는데 최근에 변경됨 -> 이런 것들은 언제 추적해야 하는가? npm outdated 해보고 버전이 바뀐 경우 체크해보기
+    buildModules: [
+        '@nuxtjs/vuetify',
     ],
     plugins: [
 
