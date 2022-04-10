ddabfcd2de3cec186ad53c4bb8a3be37040d9be7
aSpring <rnjsqhal2001@naver.com>
2022-04-10 19:37:03 +0900

    4-6. cors와 bcrypt

--- back/app.js
+++ back/app.js
@@ -1,6 +1,9 @@
 // front -> back으로 요청을 보내면 위에서부터 아래로 미들웨어를 훑으며 실행 됨
 
 const express = require('express');
+const cors = require('cors');
+const bcrypt = require('bcrypt');
+
 const db = require('./models'); // models/index.js에서 module.exports = db; -> 그 db를 불러온 것, 그 안에 User가 있음
 const app = express();
 
@@ -8,6 +11,7 @@ const app = express();
 // 서버 시작 시 app.js가 시작되면서 db, sequelize.sync()까지 같이 실행되도록
 db.sequelize.sync(); // db 시작
 
+app.use(cors('http://localhost:3000')); // () 이렇게 하면 모든 요청을 다 허용하므로 실무에서는 절대 이렇게하면 안됨 -> 정확하게 허용할 프론트 주소 적어주기
 // express는 body로 Json data를 받지못하므로 써주어야 함
 app.use(express.json()); // 요청으로 온 json data를 parsing(해석)해서 req.body에 넣어줌
 app.use(express.urlencoded({extended: false})); // form에서 action을 통해 전송할 때, 그 데이터를 해석해서 req.body에 넣어줌
@@ -24,9 +28,11 @@ app.get('/', (req, res) => {
 // 회원가입
 app.post('/user', async (req, res, next) => { // promise이기 때문에 async await를 붙여주고 -> try catch로 감싸주어야 함
     try {
+        const hash = await bcrypt.hash(req.body.password, 12); // (원문, salt -> 높일수록 좋으나, 복잡하게 암호화하기 때문에 느려질 수 있음)
         const newUser = await db.User.create({
             email: req.body.email, 
-            password: req.body.password, 
+            // password: req.body.password, 
+            password: hash, 
             nickname: req.body.nickname,
         });    
         // 200 -> 성공, 201 -> 성공적으로 생성됨 (HTTP STATUS CODE)
